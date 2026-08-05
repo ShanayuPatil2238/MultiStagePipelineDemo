5b6e6349a4d35d8351596f8f020a703832e90b23
ShanayuPatil2238 <shanayu.patil@prorigosoftware.com>
2026-08-05 14:47:00 +0530

    Initial ASP.nET Core application

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MultiStagePipelineDemo.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
